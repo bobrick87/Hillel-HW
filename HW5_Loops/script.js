870e85545fee926ae91eff49d8a123c494c3790d
bobrick87 <35877633+bobrick87@users.noreply.github.com>
2022-11-26 17:34:49 +0200

    task2

--- HW5_Loops/script.js
+++ HW5_Loops/script.js
@@ -1,12 +1,28 @@
-// 1. Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
+const task = +prompt('Обери, яку задачу будемо виконувати (1, 2, 3, 4 або 5)');
 
-for (let i = 20, j; i <= 30; i++) {
-    console.log(i);
-    j = i + 0.5;
-    if (j < 30) {
-    console.log(j)};
-}
+switch (task) {
+    case 1:
+        document.write('1. Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….). <br>');
+        for (let i = 20; i <= 30; i = i + 0.5) {
+            document.write(i + "<br>");
+        }
+        break;
+    case 2:
+        document.write('2. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів. <br>')
+        const OneDollar = +prompt("Який сьогодні курс доллара?");
+        if (isNaN(OneDollar)) {
+            alert("Можна використовувати лише цифри!");
+        } else {
+            let i = 10;
+            while (i <= 100) {
+                document.write(`${i} доларів коштують ${OneDollar * i} <br>`);
+                i = i + 10;
+            }
+        }
+        break;
+    
 
-for (let i = 20; i <= 30; i = i + 0.5) {
-    console.log(i);
+        default:
+            alert("Некоректний вибір");
+
 }
